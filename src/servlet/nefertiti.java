bcafb5474ea32ae7caa52ecc7804af6bdf3f38ae
gundamotoko <poubelleend971@gmail.com>
2016-10-29 17:39:43 -0400

    Update nefertiti.java
    Modification de la fonction get du  servlet nefertiti
                -gestion de la connexion de nouveau joueur.
                -gestion du déplacement d'un joueur

--- src/servlet/nefertiti.java
+++ src/servlet/nefertiti.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import player.Joueur;
+import player.ListeDeJoueurs;
 
 /**
  * Servlet implementation class nefertiti
@@ -13,8 +19,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/nefertiti")
 public class nefertiti extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
+	ListeDeJoueurs ldj = new ListeDeJoueurs();
+    /** 
      * @see HttpServlet#HttpServlet()
      */
     public nefertiti() {
@@ -27,7 +33,50 @@ public class nefertiti extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+        response.setCharacterEncoding( "UTF-8" );
+        //requete exemple pour creer un nouveau joueur 
+        //http://localhost:8080/CrazyM2/nefertiti?requete=nouveau&pseudo=jjj
+        String requete=request.getParameter("requete");
+        if (requete.compareTo("nouveau")==0)
+        {
+        String pseudo=request.getParameter("pseudo");
+        Joueur player = new Joueur(pseudo);
+        ldj.addJoueur(player);
+        PrintWriter out = response.getWriter();
+        out.println(ldj.allPositions());
+        }
+        //requete exemple pour changer la position du joueur 
+        //http://localhost:8080/CrazyM2/nefertiti?requete=jeu&id=1&sens=avancer&angle=droite
+        else if (requete.compareTo("jeu")==0)
+        {
+        	String id=request.getParameter("id");
+        	String sens=request.getParameter("sens");
+        	String angle=request.getParameter("angle");
+        	Joueur mon_joueur=ldj.searchJoueur(Integer.parseInt(id));
+        	if (sens.compareTo("avancer")==0)
+        	{	
+        		mon_joueur.getCar().avancer();
+        	}
+        	else if (sens.compareTo("reculer")==0)
+        	{	
+        		mon_joueur.getCar().reculer();
+        	}
+        	else if (sens.compareTo("libre")==0)
+        	{	
+        		mon_joueur.getCar().decelerer();
+        	}
+        	if (angle.compareTo("gauche")==0)
+        	{
+        		mon_joueur.getCar().tournerGauche();
+        	}
+        	else if (angle.compareTo("droite")==0)
+        	{	
+        		mon_joueur.getCar().tournerDroite();
+        	}	
+            PrintWriter out = response.getWriter();
+            out.println(ldj.allPositions());	
+        }
 	}
-
 }
