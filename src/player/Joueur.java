f6b9c83d59b3504ec9f350332698cad5f59d83c4
gundamotoko <poubelleend971@gmail.com>
2016-11-08 12:24:40 -0400

    Modification de la classe joueur
    Le joueur n'a plus de voiture mais a un avion.

--- src/player/Joueur.java
+++ src/player/Joueur.java
@@ -1,4 +1,5 @@
 package player;
+import AirCraft.Avion;
 import car.Voiture;
 
 
@@ -6,25 +7,23 @@ public class Joueur {
     private static int increment = 0;
     private final int id;
     private final String pseudo;
-    private final Voiture car;
-
+    private final Avion plane;
     public Joueur (String pseudo) {
         id = ++increment;
         this.pseudo = pseudo;
-        this.car = new Voiture();
+        this.plane=new Avion("MIG-51S",0,0,100,100);
     }
 
     public int getId() {
         return this.id;
     }
-    
-    public Voiture getCar()
+    public Avion GetAvion()
     {
-    	return this.car;
+    	return this.plane;
     }
-    
+ 
     public double[] getPosition() {
-        double positions[] = {this.car.getX(), this.car.getY(), this.car.getAngle()};
+        double positions[] = {this.plane.GetX(), this.plane.GetY(), this.plane.getAngle()};
         return positions;
     }
 }
