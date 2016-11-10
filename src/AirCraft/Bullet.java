613a2109273d54a05017784f56462c42f4064bc7
gundamotoko <poubelleend971@gmail.com>
2016-11-10 10:14:56 -0400

    Update Bullet.java
    La classe Bullet représente les balles qui sortent  de l'avion quand celui-ci tire.
    
    Elle s'actualise grace à la méthode actualiser qui fera avancer la balle chaque fois que le temps sera multiple de 2.

--- src/AirCraft/Bullet.java
+++ src/AirCraft/Bullet.java
@@ -5,20 +5,28 @@ import Base.Graphique;
 public class Bullet extends Graphique {
 	private int degats;
 	private int vitesse;
-	public Bullet(String nom, int x, int y, int width, int heigth) {
+	public Bullet(String nom, int x, int y, int width, int heigth,double angle) {
 		super(nom, x, y, width, heigth);
+		this.vitesse=10;
+		this.degats=5;
+		this.SetAngle(angle);
 		
 	}
-	
+	//Actualise la position de la balle selon le temps
 	public void Actualiser(long time)
 	{
 		
+	    // Si le temps est un multiple de deux alors la balle avance
+		if (time%2==0)
+		{
+			this.Avancer();
+		}
 	}
 	
 	public void Avancer()
 	{
-		this.SetX(this.GetX()+ (int)(vitesse*Math.cos(this.getAngle()*Math.PI/180)));
-		this.SetY(this.GetY()+ (int)(vitesse*Math.sin(this.getAngle()*Math.PI/180)));
+		this.SetX(this.GetX()- (int)(vitesse*Math.cos(this.getAngle()*Math.PI/180)));
+		this.SetY(this.GetY()- (int)(vitesse*Math.sin(this.getAngle()*Math.PI/180)));
 	}
 
 }
