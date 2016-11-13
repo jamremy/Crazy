cb1f9dfb640221a86940c3868b63456e52d7df35
gundamotoko <poubelleend971@gmail.com>
2016-11-13 09:31:49 -0400

    Update Ennemi.java
    Ajout de la mhode tirer qui permet a l'ennemi de tirer un ou plusieur BulletEnnemi  . 
    Retrait de la méthode blesser qui a été remonté dans la classe aircraft afin que tout les avion puissent être blessé et pas seulement les avion ennemi.
    Modification de la méthode actualiser qui lorsce qu'elle est appeler fait l'ennemi tirer.

--- src/VaisseauEnnemi/Ennemi.java
+++ src/VaisseauEnnemi/Ennemi.java
@@ -2,6 +2,7 @@ package VaisseauEnnemi;
 
 import AirCraft.Avion;
 import AirCraft.Bullet;
+import Base.ListeDeGraphiques;
 
 public class Ennemi extends Avion{
 
@@ -13,6 +14,7 @@ public class Ennemi extends Avion{
 			this.SetAttaque(20);
 			this.SetDefense(10);
 			this.SetVitesse(10);
+			this.SetFrequenceActualisation(3000);
 			
 		}
 		else if (Nom.compareTo("ennemi2")==0)
@@ -26,15 +28,28 @@ public class Ennemi extends Avion{
 		
 		// TODO Auto-generated constructor stub
 	}
-	public void Blesser(Bullet balle)
-	{
 	
-			this.SetVie(this.GetVie()-balle.GetDegat());
+	
+	public void tirer(ListeDeGraphiques ldg)
+	{
+		
+		//Definition d'un comportement  de tire pour lennemi1
+		if (this.GetNom().compareTo("ennemi1")==0)
+		{
+		ldg.add(new BulletEnnemi(this.GetX(),this.GetY(),-180));
+		ldg.add(new BulletEnnemi(this.GetX(),this.GetY(),-90));
+		ldg.add(new BulletEnnemi(this.GetX(),this.GetY(),-45));
+		}
+		//Definition d'un comportement  de tire pour lennemi2
+		else if (this.GetNom().compareTo("ennemi1")==0)
+		{
+			
+		}
 	}
-	
-	public void Actualiser(long time)
+	public void Actualiser(long time,ListeDeGraphiques ldg)
 	{
-	
+				this.tirer(ldg);
+				this.SetTimeLastActualisation(time);	
 	}
 
 }
