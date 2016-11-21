06b8d6843942ebf62d2eb0356a14e9ba46fc3e43
gundamotoko <poubelleend971@gmail.com>
2016-11-20 21:50:27 -0400

    Update Avion.java
    Ajout du champs MesBonus de type ListeDeBonus qui sert a contenir les bonus que l'avion possède.
    Modification de la méthode tirer qui maintenant prend en compte les bonus de lavion et en cas de bonus Ballefoistrois tire trois balle ou encore en cas de bonus BallePerforante  tire une balle preforante de plus grande puissance.
    
    Modification des méthode avancer et reculer qui maintenant empêche lavion de sortir de lécran.
    
    Modification de la méthode Actualiser() de tels sorte a ce quelle prenne en compte les bonus de l'avion telles que la santé et le bouclier.
    
    Modification de la méthode blesser qui maintenant ne blesse plus lavion lorsceque celui-ci porte un bouclier et qui met la vie  de celui-ci a 0 en cas de contact avec une DeathBullet (La deathBullet est utilisé pour tuer lennemis lorsceque celui ci rentre en collision avec l'avion héros).

--- src/AirCraft/Avion.java
+++ src/AirCraft/Avion.java
@@ -1,40 +1,53 @@
 package AirCraft;
 
+import java.util.Iterator;
+
 import Base.Graphique;
+import Base.ListeDeGraphiques;
+import Bonus.ListeDeBonus;
+import Bonus.bonus;
 
 public class Avion extends Graphique{
 	public static final double ANGLE = 2;
 	
 	private double vitesse;
 	private double  vie;
+	private String Bonus;
 	private int attaque;
+	private ListeDeBonus MesBonus=new ListeDeBonus();
 	private int defense;
 	public Avion(String Nom, int x, int y, int width,int height) {
 		super(Nom,x,y,width,height);
 		this.SetAngle(0);
+		/*this.SetX(-50);
+		this.SetY(50);*/
 		if (Nom.equals("MIG-51S"))
 		{
-			this.vie=10;
+			this.vie=1000;
 			this.vitesse=10;
 			this.attaque=15;
 			this.defense=30;
 		}
 		else if (Nom.equals("Su-55"))
 		{
-			this.vie=10;
+			this.vie=1000;
 			this.vitesse=10;
 			this.attaque=15;
 			this.defense=30;
 		}
 		else if (Nom.equals("Su-37K"))
 		{
-			this.vie=10;
+			this.vie=1000;
 			this.vitesse=10;
 			this.attaque=15;
 			this.defense=30;
 		}
 	}
 
+	public ListeDeBonus GetMesBonus()
+	{
+		return MesBonus;
+	}
 	public void tournerGauche() {
 		
 		this.SetAngle(this.getAngle()+ANGLE);
@@ -65,15 +78,109 @@ public class Avion extends Graphique{
 	}
 	public void Blesser(Bullet balle)
 	{
-	
+		if (this.MesBonus.size()!=0)
+		{
+			if (this.MesBonus.get(0).GetNom().compareTo("Bouclier")!=0)
+			{
+				this.SetVie(this.GetVie()-balle.GetDegat());
+			}
+		}
+		else 
+		{
 			this.SetVie(this.GetVie()-balle.GetDegat());
+		}
+		if (balle.GetNom().compareTo("DeathBullet")==0)
+		{
+			this.vie=0;
+		}
+		
 	}
 	public void avancer() {
-		this.SetX(this.GetX()+ (int)(vitesse*Math.cos(this.getAngle()*Math.PI/180)));
-		this.SetY(this.GetY()+ (int)(vitesse*Math.sin(this.getAngle()*Math.PI/180)));
+		
+			
+			
+			int new_x=this.GetX()+ (int)(vitesse*Math.cos(this.getAngle()*Math.PI/180));
+			if (new_x>0 && new_x<600)
+			{
+				this.SetX(new_x);
+			}
+			int new_y=this.GetY()+ (int)(vitesse*Math.sin(this.getAngle()*Math.PI/180));
+			
+			if (new_y>0 && new_y<700)
+			{
+			this.SetY(new_y);
+			}
 	}
-	public void reculer() {
-		this.SetX(this.GetX()- (int)(vitesse*Math.cos(this.getAngle()*Math.PI/180)));
-		this.SetY(this.GetY()- (int)(vitesse*Math.sin(this.getAngle()*Math.PI/180)));
+	public void Actualiser()
+	{
+		int i=0;
+		for (Iterator<bonus> iterator = MesBonus.iterator(); iterator.hasNext(); ) {
+			bonus b = iterator.next();
+			
+			if (b.GetDuree()==0 )
+			{
+				iterator.remove();
+				
+			}
+			else 
+			{
+				
+					if (b.GetNom().compareTo("Sante")==0)
+					{
+						this.vie+=30;
+						b.use();
+					}
+					else if (b.GetNom().compareTo("Bouclier")==0)
+					{
+						b.use();
+					}
+					
+			}
+		}
+	}
+	public void tirer (ListeDeGraphiques ldg)
+	{
+		if (this.MesBonus.size()!=0)
+		{
+			for (bonus b: MesBonus)
+			{
+			if  (b.GetNom().compareTo("Ballefoistrois")==0)
+			{
+				ldg.add(new Bullet("Bullet",this.GetX()-(int)(50*Math.sin(this.getAngle()*Math.PI/180)),this.GetY()-(int)(50*Math.cos(this.getAngle()*Math.PI/180)),21,21,this.getAngle())); 
+				ldg.add(new Bullet("Bullet",this.GetX(),this.GetY(),21,21,this.getAngle())); 
+				
+				ldg.add(new Bullet("Bullet",this.GetX()+(int)(50*Math.sin(this.getAngle()*Math.PI/180)),this.GetY()+(int)(50*Math.cos(this.getAngle()*Math.PI/180)),21,21,this.getAngle())); 
+				b.use();
+			}
+			else if (b.GetNom().compareTo("BallePerforante")==0)
+			{
+				ldg.add(new Bullet("BallePerforante",this.GetX(),this.GetY(),21,21,this.getAngle())); 
+				b.use();
+			}
+			else 
+			{
+				ldg.add(new Bullet("Bullet",this.GetX(),this.GetY(),21,21,this.getAngle())); 
+			}
+		}
+		}
+		
+		else
+		{
+		ldg.add(new Bullet("Bullet",this.GetX(),this.GetY(),21,21,this.getAngle())); 
+		}
+	}
+	
+	public void reculer()
+	{	
+			int new_x=this.GetX()- (int)(vitesse*Math.cos(this.getAngle()*Math.PI/180));
+			if (new_x>0 && new_x<600)
+			{
+				this.SetX(new_x);
+			}
+			int new_y=this.GetY()- (int)(vitesse*Math.sin(this.getAngle()*Math.PI/180));
+			if (new_y>0 && new_y<700)
+			{
+			this.SetY(new_y);
+			}	
 	}
 }
