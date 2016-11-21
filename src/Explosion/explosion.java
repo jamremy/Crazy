5e4abc777048cf1f9f10979ff1cdbacba09da8ac
gundamotoko <poubelleend971@gmail.com>
2016-11-20 20:34:47 -0400

    Create explosion.java
    Cette classe se charge de générer et de gérer l'animation d'explosion qui se produit lorsce qu'un avion n'a plus de vie. L'explosion correspondante qui contient 32  images se trouve dans le dossier image/explosion.

---
+++ src/Explosion/explosion.java
@@ -0,0 +1,40 @@
+package Explosion;
+
+import Base.Graphique;
+
+public class explosion extends Graphique{
+
+	private int NumImage=0;
+	private int NbImage; //Nombre d'image  que 
+	public explosion(String nom, int x, int y, int width, int height) {
+		super(nom, x, y, width, height);
+		this.SetFrequenceActualisation(100);
+		if (nom.compareTo("Explosion1")==0)
+		{
+			this.NbImage=31;
+		}
+		// TODO Auto-generated constructor stub
+	}
+	public int GetNumImage ()
+	{
+		return NumImage;
+	}
+	
+	public int GetNbImage ()
+	{
+		return NbImage;
+	}
+	
+	public void Actualiser(long time)
+	{
+	    // Si le temps est un multiple de deux alors la balle avance
+		if (this.NumImage<this.NbImage)
+		{
+			this.NumImage+=1;
+		}
+			this.SetTimeLastActualisation(time);
+		
+	}
+	
+	
+}
