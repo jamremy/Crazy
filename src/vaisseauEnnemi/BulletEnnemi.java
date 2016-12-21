e01542d410c26467b854e1c1e8cd2c3662965a19
gundamotoko <poubelleend971@gmail.com>
2016-12-20 21:29:25 -0400

    Update BulletEnnemi.java
    Correction de l'erreur du constructeur

--- src/vaisseauEnnemi/BulletEnnemi.java
+++ src/vaisseauEnnemi/BulletEnnemi.java
@@ -5,7 +5,7 @@ import airCraft.Bullet;
 public class BulletEnnemi extends Bullet {
 
 	public BulletEnnemi(int x, int y, double angle) {
-		super("BulletEnnemi", x, y, 40, 40, angle);
+		super("BulletEnnemi", x, y, 40, 40, angle,null);
 		// TODO Auto-generated constructor stub
 		this.SetFrequenceActualisation(10);
 		this.SetVitesse(3);
